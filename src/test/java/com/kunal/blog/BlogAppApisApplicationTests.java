8c1673d4da26d6b28454859ad700456bb52625fd
Kunal <kunalgrv47@gmail.com>
2024-02-27 18:21:25 +0530

    First commit for project. Uploading starter project

---
+++ src/test/java/com/kunal/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
